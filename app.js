c3ff8e5ed55d1d937f460673742d84357511ee02
sureshpriyanka <sureshpriyanka@users.noreply.github.com>
2017-01-11 14:39:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,3 @@
+var employee={name:"PRIYA",Age:20,salary:"Rs.50000",address:{city:"Tirupur",state:"Tamilnadu",pincode:604602}};
+
+console.log("NAME:"+employee.name+"\nAGE:"+employee.Age+"\nsalary:"+employee.salary+"\nCITY:"+employee.address.city+"\nstate:"+employee.address.state+"\npincode:"+employee.address.pincode);
